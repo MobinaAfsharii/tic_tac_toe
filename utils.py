0dc23bf0e52bbdb478062b3ecadd8eb81b67529f
Mobina Afshari <138586209+MobinaAfsharii@users.noreply.github.com>
2025-04-25 14:10:51 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -34,3 +34,62 @@ def display_scores(scores: ScoreDict) -> None:
             border_style="blue",
         )
     )
+
+
+
+def clear_screen() -> None:
+    _ = os.system("cls" if os.name == "nt" else "clear")
+
+
+def print_board(
+    board: Board,
+    player_marker: PlayerMarker,
+    ai_marker: PlayerMarker,
+    winning_combo: tuple[int, int, int] | None = None,
+) -> None:
+    clear_screen()
+    print(
+        Panel(
+            "[bold magenta]Tic-Tac-Toe AI[/bold magenta]",
+            expand=False,
+            border_style="magenta",
+        )
+    )
+
+    grid = "\n[{}]{}---{}---{}[/]\n".format(BOARD_COLOR, "+", "+", "+")
+    for i in range(0, 9, 3):
+        row_cells: list[str] = []
+        for j in range(3):
+            idx = i + j
+            cell_content = board[idx]
+            cell_style = BOARD_COLOR
+
+            if cell_content == player_marker:
+                cell_style = PLAYER_COLOR
+            elif cell_content == ai_marker:
+                cell_style = AI_COLOR
+            else:
+
+                cell_content = str(idx + 1)
+
+            if winning_combo and idx in winning_combo:
+                cell_style = f"{WIN_COLOR} on {BOARD_COLOR}"
+
+            row_cells.append(
+                f"[on {BOARD_COLOR}][{cell_style}]{' ' + cell_content + ' '}[/][on {BOARD_COLOR}]"
+            )
+
+        grid += "[{}]{}[/]{}[{}]{}[/]{}[{}]{}[/]\n".format(
+            BOARD_COLOR,
+            "|",
+            row_cells[0],
+            BOARD_COLOR,
+            "|",
+            row_cells[1],
+            BOARD_COLOR,
+            "|",
+        )
+        if i < 6:
+            grid += "[{}]{}---{}---{}[/]\n".format(BOARD_COLOR, "+", "+", "+")
+
+    print(grid)
